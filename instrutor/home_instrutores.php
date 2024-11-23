1927675feffb211a2ec23fd310cb8f295a1f4d93
joaovictOr-SR <ruasjoaovictorsantos@gmail.com>
2024-11-23 15:45:31 -0300

    home instrutor estilizada!

--- instrutor/home_instrutores.php
+++ instrutor/home_instrutores.php
@@ -3,20 +3,27 @@ include '../conexao.php';
 include '../validacao_instrutor.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login instrutor</title>
+    <title>Home Instrutor</title>
+    <link rel="stylesheet" href="../assets/css/home_instrutor.css"> <!-- Referência ao CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="..\assets\img\logourl.png" type="image/x-icon">
 </head>
 <body>
-
-<h1>O que voce deseja fazer</h1>
-    <a href="editar-alunos/alterar_alunos.php">alteração dos alunos</a>
-    <a href="editar-equipamentos/alterar_equipamentos.php">alteração dos equipamentos</a>
-    <a href="editar-series/alterar_series.php">alteração das series</a>
-    <a href="dashboard_analitico.php">dashboard analitico</a>
-    <a href="gerar_relatorio.php">gerar relatorio</a>
-    <a href="../logout.php">sair</a>
+    <div class="container">
+        <h1>Bem-vindo, Instrutor!</h1>
+        <p>O que você deseja fazer?</p>
+        <div class="menu">
+            <a class="btn" href="editar-alunos/alterar_alunos.php">Alteração dos Alunos</a>
+            <a class="btn"href="editar-equipamentos/alterar_equipamentos.php">Alteração dos Equipamentos</a>
+            <a class="btn"href="editar-series/alterar_series.php">Alteração das Séries</a>
+            <a class="btn"href="dashboard_analitico.php">Dashboard Analítico</a>
+            <a class="btn"  href="index_relatorio.php">Gerar Relatório</a>
+            <a href="../logout.php" class="logout">Sair</a>
+        </div>
+    </div>
 </body>
 </html>
